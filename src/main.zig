96997e4030210497601dd87a8b33dcc28453b5ae
SultansOfCode <sultans.of.code@gmail.com>
2025-02-20 01:00:01 -0300

    Converting from u32 to i32

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,25 @@
 const std = @import("std");
 const rl = @import("raylib");
 
-const FONT_SIZE: u32 = 20;
-const SCREEN_LINES: u32 = 32;
-const SCREEN_COLUMNS: u32 = 74;
+const FONT_SIZE: i32 = 20;
+const SCREEN_LINES: i32 = 32;
+const SCREEN_COLUMNS: i32 = 74;
 
 var fontWidth: f32 = undefined;
 var fontHeight: f32 = undefined;
 
-var screenWidth: u32 = undefined;
-var screenHeight: u32 = undefined;
+var screenWidth: i32 = undefined;
+var screenHeight: i32 = undefined;
 
-var lineHeight: u32 = undefined;
+var lineHeight: i32 = undefined;
 
 var font: rl.Font = undefined;
-var selectedLine: u32 = 0;
+var selectedLine: i32 = 0;
 
 const ROM: type = struct {
     data: []u8,
-    lines: u32,
-    address: u32,
+    lines: i32,
+    address: i32,
     symbols: [256]u8,
     loaded: bool,
 
@@ -29,7 +29,7 @@ const ROM: type = struct {
 
         for (0..256) |i| {
             if ((i >= 'A' and i <= 'Z') or (i >= 'a' and i <= 'z') or (i >= '0' and i <= '9')) {
-                symbols[i] = @intCast(i);
+                symbols[i] = @as(u8, @intCast(i));
             }
         }
 
@@ -65,8 +65,8 @@ pub fn drawTextCustom(text: [*:0]const u8, x: i32, y: i32, color: rl.Color) void
 }
 
 pub fn drawFrame() anyerror!void {
-    const viewTopLine: u32 = rom.address / 16;
-    const viewBottomLine: u32 = viewTopLine + SCREEN_LINES - 1;
+    const viewTopLine: i32 = @divFloor(rom.address, 16);
+    const viewBottomLine: i32 = viewTopLine + SCREEN_LINES - 1;
 
     var buffer = std.ArrayList(u8).init(std.heap.page_allocator);
 
@@ -76,46 +76,51 @@ pub fn drawFrame() anyerror!void {
     rl.clearBackground(rl.Color.dark_gray);
 
     // Draw top bar
-    rl.drawRectangle(0, 0, @intCast(screenWidth), @intCast(lineHeight), rl.Color.white);
+    rl.drawRectangle(0, 0, screenWidth, lineHeight, rl.Color.white);
 
     drawTextCustom(" Offset  00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 0123456789ABCDEF", 0, 0, rl.Color.black);
 
     // Draw status bar
-    rl.drawRectangle(0, @intCast(screenHeight - lineHeight), @intCast(screenWidth), @intCast(lineHeight), rl.Color.white);
+    rl.drawRectangle(0, screenHeight - lineHeight, screenWidth, lineHeight, rl.Color.white);
 
     try buffer.appendSlice(" Line: ");
     try buffer.writer().print("{d}/{d} ({d:.1}%)", .{ selectedLine + 1, rom.lines, (@as(f32, @floatFromInt(selectedLine)) + 1.0) * 100.0 / @as(f32, @floatFromInt(rom.lines)) });
 
-    drawTextCustom(@ptrCast(buffer.items), 0, @intCast(screenHeight - lineHeight), rl.Color.black);
+    drawTextCustom(@ptrCast(buffer.items), 0, screenHeight - lineHeight, rl.Color.black);
 
     // Draw selected highlight
     if (selectedLine >= viewTopLine and selectedLine <= viewBottomLine) {
-        rl.drawRectangle(0, @intCast((selectedLine - viewTopLine + 1) * lineHeight), @intCast(screenWidth), @intCast(lineHeight), rl.Color.light_gray);
+        rl.drawRectangle(0, (selectedLine - viewTopLine + 1) * lineHeight, screenWidth, lineHeight, rl.Color.light_gray);
     }
 
     // Draw contents
     buffer.clearRetainingCapacity();
 
     for (0..SCREEN_LINES) |i| {
-        const addr = rom.address + 0x10 * i;
+        const address: i32 = rom.address + 0x10 * @as(i32, @intCast(i));
+        var byteIndex: usize = undefined;
 
-        if (addr > rom.data.len) {
+        if (address > rom.data.len) {
             continue;
         }
 
-        try buffer.writer().print("{X:0>8} ", .{addr});
+        try buffer.writer().print("{X:0>8} ", .{address});
 
         for (0..16) |j| {
-            if (addr + j < rom.data.len) {
-                try buffer.writer().print("{X:0>2} ", .{rom.data[addr + j]});
+            byteIndex = @as(usize, @intCast(address)) + j;
+
+            if (byteIndex < rom.data.len) {
+                try buffer.writer().print("{X:0>2} ", .{rom.data[byteIndex]});
             } else {
                 try buffer.appendSlice("   ");
             }
         }
 
         for (0..16) |j| {
-            if (addr + j < rom.data.len) {
-                try buffer.append(rom.symbols[rom.data[addr + j]]);
+            byteIndex = @as(usize, @intCast(address)) + j;
+
+            if (byteIndex < rom.data.len) {
+                try buffer.append(rom.symbols[rom.data[byteIndex]]);
             } else {
                 try buffer.append(' ');
             }
@@ -123,7 +128,7 @@ pub fn drawFrame() anyerror!void {
 
         try buffer.append(0);
 
-        drawTextCustom(@ptrCast(buffer.items), 0, @intCast((i + 1) * lineHeight), if (viewTopLine + i == selectedLine) rl.Color.black else rl.Color.light_gray);
+        drawTextCustom(@ptrCast(buffer.items), 0, @as(i32, @intCast(i + 1)) * lineHeight, if (viewTopLine + @as(i32, @intCast(i)) == selectedLine) rl.Color.black else rl.Color.light_gray);
 
         buffer.clearRetainingCapacity();
     }
@@ -150,7 +155,7 @@ pub fn main() anyerror!u8 {
 
     lineHeight = @intFromFloat(@round(fontMeasurements.y));
 
-    rl.setWindowSize(@intCast(screenWidth), @intCast(screenHeight));
+    rl.setWindowSize(screenWidth, screenHeight);
 
     rom = try ROM.init("resources/test.txt");
     defer rom.deinit();
@@ -165,7 +170,7 @@ pub fn main() anyerror!u8 {
         rl.beginDrawing();
         defer rl.endDrawing();
 
-        const viewTopLine = rom.address / 16;
+        const viewTopLine = @divFloor(rom.address, 16);
         const viewBottomLine = viewTopLine + SCREEN_LINES - 1;
 
         if (rl.isKeyPressed(rl.KeyboardKey.down)) {
@@ -185,39 +190,39 @@ pub fn main() anyerror!u8 {
                 }
             }
         } else if (rl.isKeyPressed(rl.KeyboardKey.page_down)) {
-            const newLine: i32 = @min(@as(i32, @intCast(rom.lines)) - 1, @as(i32, @intCast(selectedLine)) + @as(i32, @intCast(SCREEN_LINES)));
-            const difference: i32 = newLine - @as(i32, @intCast(selectedLine));
+            const newLine: i32 = @min(rom.lines - 1, selectedLine + SCREEN_LINES);
+            const difference: i32 = newLine - selectedLine;
 
-            selectedLine = @as(u32, @intCast(newLine));
+            selectedLine = newLine;
 
-            var newAddress: i32 = @as(i32, @intCast(rom.address));
+            var newAddress: i32 = rom.address;
 
             if (difference >= SCREEN_LINES) {
                 newAddress += difference * 16;
             } else if (rom.lines > SCREEN_LINES) {
-                newAddress = (@as(i32, @intCast(rom.lines)) - SCREEN_LINES - 1) * 16;
+                newAddress = (rom.lines - SCREEN_LINES - 1) * 16;
             }
 
             try std.io.getStdOut().writer().print("NL: {d} D: {d} RA: {d} NA: {d}\n", .{ newLine, difference, rom.address, newAddress });
 
-            rom.address = @as(u32, @intCast(newAddress));
+            rom.address = newAddress;
         } else if (rl.isKeyPressed(rl.KeyboardKey.page_up)) {
-            const newLine: i32 = @max(@as(i32, @intCast(selectedLine)) - @as(i32, @intCast(SCREEN_LINES)), 0);
-            const difference: i32 = @as(i32, @intCast(selectedLine)) - newLine;
+            const newLine: i32 = @max(selectedLine - SCREEN_LINES, 0);
+            const difference: i32 = selectedLine - newLine;
 
-            selectedLine = @as(u32, @intCast(newLine));
+            selectedLine = newLine;
 
             var newAddress: i32 = undefined;
 
             if (difference >= SCREEN_LINES) {
-                newAddress = @as(i32, @intCast(rom.address)) - (difference * 16);
+                newAddress = rom.address - (difference * 16);
             } else {
                 newAddress = 0;
             }
 
             try std.io.getStdOut().writer().print("NL: {d} D: {d} RA: {d} NA: {d}\n", .{ newLine, difference, rom.address, newAddress });
 
-            rom.address = @as(u32, @intCast(newAddress));
+            rom.address = newAddress;
         }
 
         try drawFrame();
